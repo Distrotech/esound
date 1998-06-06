193a1fd2ab0eb65dff0414c6e52c6e4bbbc54385
Frank Belew (Myth) <frb@src.gnome.org>
1998-06-06 03:38:06 +0000

    autogen actually works now

--- autogen.sh
+++ autogen.sh
@@ -31,6 +31,11 @@ if test "$DIE" -eq 1; then
 	exit 1
 fi
 
+aclocal $ACLOCAL_FLAGS
+autoheader
+autoconf
+automake --gnu --add-missing
+
 if test -z "$*"; then
 	echo "I am going to run ./configure with no arguments - if you wish "
         echo "to pass any to it, please specify them on the $0 command line."
