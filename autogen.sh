1613ccf6e4e7f63ad1e2e7868fb22c92a48b1a3e
Frank Belew (Myth) <frb@src.gnome.org>
1998-10-19 19:28:15 +0000

    fixed lines that said GTK+ to say esound (no this wasn't stolen from anyone)

--- autogen.sh
+++ autogen.sh
@@ -5,7 +5,7 @@ DIE=0
 
 (autoconf --version) < /dev/null > /dev/null 2>&1 || {
 	echo
-	echo "You must have autoconf installed to compile GTK+."
+	echo "You must have autoconf installed to compile esound."
 	echo "Download the appropriate package for your distribution,"
 	echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
 	DIE=1
@@ -13,7 +13,7 @@ DIE=0
 
 (libtool --version) < /dev/null > /dev/null 2>&1 || {
 	echo
-	echo "You must have libtool installed to compile GTK+."
+	echo "You must have libtool installed to compile esound."
 	echo "Get ftp://alpha.gnu.org/gnu/libtool-1.0h.tar.gz"
 	echo "(or a newer version if it is available)"
 	DIE=1
@@ -21,7 +21,7 @@ DIE=0
 
 (automake --version) < /dev/null > /dev/null 2>&1 || {
 	echo
-	echo "You must have automake installed to compile GTK+."
+	echo "You must have automake installed to compile esound."
 	echo "Get ftp://ftp.cygnus.com/pub/home/tromey/automake-1.2d.tar.gz"
 	echo "(or a newer version if it is available)"
 	DIE=1
