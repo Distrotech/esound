b14ae3c11d30fe41b903c1ca5ef9fb95a991e010
Frédéric Crozat <fcrozat@src.gnome.org>
2002-06-19 14:01:28 +0000

    revert locking of audio stream patch, this patch breaks esd when used in
    * players.c: (read_player): revert locking of audio stream patch, this
    patch breaks esd when used in TCP mode on localhost (Santiago Otero)

--- players.c
+++ players.c
@@ -302,12 +302,6 @@ int read_player( esd_player_t *player )
 		if ( actual == 0 
 		     || ( actual < 0 && errno != EAGAIN && errno != EINTR ) )
 		    return -1;
-		/* check for last frame */
-		if ( actual < player->buffer_length - player->actual_length )
-		{
-			player->actual_length += actual;
-			break;
-		}
 		/* more data, save how much we got */
 		player->actual_length += actual;
 	    } while (player->actual_length < player->buffer_length);
