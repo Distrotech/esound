18734a84bf5413365ab32f80ca38d8788a86c92d
David Schleef <ds@src.gnome.org>
2005-06-18 00:54:24 +0000

    ha ha funny use gnome-autogen.sh esound, not esd
    * README: ha ha funny
    * autogen.sh: use gnome-autogen.sh
    * configure.ac: esound, not esd

--- autogen.sh
+++ autogen.sh
@@ -4,50 +4,23 @@
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-DIE=0
+PKG_NAME="ESound"
 
-(autoconf --version) < /dev/null > /dev/null 2>&1 || {
-	echo
-	echo "You must have autoconf installed to compile esound."
-	echo "Download the appropriate package for your distribution,"
-	echo "or get the source tarball at http://ftp.gnu.org/pub/gnu/automake/"
-	DIE=1
+REQUIRED_AUTOCONF_VERSION=2.58
+REQUIRED_AUTOMAKE_VERSION=1.6
+ACLOCAL_FLAGS="-I $srcdir/m4"
+
+
+(test -f $srcdir/configure.ac) || {
+    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
+    echo " top-level directory"
+    exit 1
 }
 
-(libtool --version) < /dev/null > /dev/null 2>&1 || {
-	echo
-	echo "You must have libtool installed to compile esound."
-	echo "Get it from http://ftp.gnu.org/pub/gnu/libtool/"
-	DIE=1
+which gnome-autogen.sh || {
+    echo "You need to install gnome-common from the GNOME CVS"
+    exit 1
 }
 
-(automake --version) < /dev/null > /dev/null 2>&1 || {
-	echo
-	echo "You must have automake installed to compile esound."
-	echo "Get http://ftp.gnu.org/pub/gnu/automake/"
-	echo "(or a newer version if it is available)"
-	DIE=1
-}
+USE_GNOME2_MACROS=1 . gnome-autogen.sh
 
-if test "$DIE" -eq 1; then
-	exit 1
-fi
-
-THEDIR="`pwd`"
-cd $srcdir
-aclocal -I m4 $ACLOCAL_FLAGS || exit 1
-libtoolize --force || exit 1
-autoheader || exit 1
-autoconf || exit 1
-automake --gnu --add-missing || exit 1
-cd "$THEDIR"
-
-if test -z "$*"; then
-	echo "I am going to run ./configure with no arguments - if you wish "
-        echo "to pass any to it, please specify them on the $0 command line."
-fi
-
-$srcdir/configure --enable-maintainer-mode "$@"
-
-echo 
-echo "Now type 'make' to compile esound."
