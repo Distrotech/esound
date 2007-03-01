9b02c0e261fda23deeacc00199789df660e5158c
Kjartan Maraas <kmaraas@gnome.org>
2007-03-01 02:27:37 +0000

    Fix 100% CPU problem introduced in 0.2.37. Patch from Joe Marcus Clarke.
    2007-03-01  Kjartan Maraas  <kmaraas@gnome.org>
    
    	* players.c: (read_player): Fix 100% CPU problem introduced
    	in 0.2.37. Patch from Joe Marcus Clarke. Closes bug #412951.
    
    svn path=/trunk/; revision=485

--- players.c
+++ players.c
@@ -310,13 +310,14 @@ int read_player( esd_player_t *player )
 			      player->data_buffer + player->actual_length, 
 			      player->buffer_length - player->actual_length,
 			      actual, "str rd" );
-		if (bytes_read < player->buffer_length - player->actual_length)
-			break;
-		
 		/* check for end of stream */
 		if ( actual == 0 
 		     || ( actual < 0 && errno != EAGAIN && errno != EINTR ) )
 		    return -1;
+
+		if (bytes_read < player->buffer_length - player->actual_length)
+			break;
+		
 		/* more data, save how much we got */
 		if ( actual > 0 )
 		   player->actual_length += actual;
